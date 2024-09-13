864b9b5ee22b8de7bebedf302560618bd547a502
techupskills <82771267+techupskills@users.noreply.github.com>
2024-09-13 01:33:00 +0000

    update

--- wip/setup-neo4j.sh
+++ wip/setup-neo4j.sh
@@ -6,11 +6,11 @@ pip install neo4j
 
 wget -O - https://debian.neo4j.com/neotechnology.gpg.key | sudo apt-key add -
 echo 'deb https://debian.neo4j.com stable 5' | sudo tee /etc/apt/sources.list.d/neo4j.list
-sudo apt-get update
-sudo apt-get install neo4j
+sudo apt-get -y update
+sudo apt-get -y install neo4j
 sudo mv /var/lib/neo4j/labs/apoc-5.23.0-core.jar /var/lib/neo4j/plugins/
 echo dbms.security.procedures.unrestricted=algo.*,apoc.* | sudo tee /etc/neo4j/neo4j.conf
-neo4j-admin dbms set-initial-password admin001
+sudo neo4j-admin dbms set-initial-password admin001
 
 sudo service neo4j start
 sudo service neo4j status
